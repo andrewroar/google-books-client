acf3f51d2b7d86fdf508ca2219e0124b0c6628b4
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-09-07 21:56:15 +0100

    Updates

---
+++ precache-manifest.cf3d6d36d6ec745c349b7387af69f980.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34215fbbe917f793ef6d2aa0f3f143eb",
+    "url": "/google-books-client/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5505472048d7b7859a3",
+    "url": "/google-books-client/static/js/main.69531a7f.chunk.js"
+  },
+  {
+    "revision": "2f4606b7ccc9863de05f",
+    "url": "/google-books-client/static/js/runtime-main.7a2912b8.js"
+  }
+]);
